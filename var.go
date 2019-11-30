d0373295d79e1c01a05eac7391dc8a4e7263484c
astiktyagi <astiktyagi@gmail.com>
2019-12-01 00:35:33 +0530

    the var keyword

--- var.go
+++ var.go
@@ -2,10 +2,25 @@ package main
 
 import "fmt"
 
+/* false for booleans, 0 for numeric types,
+"" for strings, and
+nil for pointers, functions, interfaces, slices, channels, and maps. */
+
 func main() {
 
-	// short declaration operator
-	// declares and assigns the vairable
+	/* short declaration operator
+	declares and assigns the vairable*/
 	x := 42
 	fmt.Println(x)
+
+	/* var keyword
+	can be declared outside function body */
+	var y = 50
+	fmt.Println(y)
+
+	/* declare a variable with identifier
+	assigns the ZERO value of type to the value*/
+	var z []int
+
+	fmt.Println(z)
 }
